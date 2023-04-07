21d6c5eb30aedb7fc0a2eeb3f991bd69873925a0
xbwkkwz <xbwkk@hotmail.com>
2023-04-06 23:37:44 -0400

    fix like bug

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 // mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
+// const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/tuiter';
 mongoose.connect(CONNECTION_STRING);
 
 
